31052c2cdef3168d0e78579871dfa30070f36bb5
CeePlusPlus <sagarss@hotmail.com>
2017-07-12 22:37:15 +0530

    calculation method added

--- src/paintcostestimator/PaintCostEstimator.java
+++ src/paintcostestimator/PaintCostEstimator.java
@@ -1,7 +1,7 @@
 package paintcostestimator;
 
 import java.util.Scanner;
-//hi
+
 /**
  *
  * @author MANOJ
@@ -15,23 +15,34 @@ public class PaintCostEstimator {
     int No_of_rooms = 0;
     double[] length = new double[MAX_SIZE];
     double[] width = new double[MAX_SIZE];
+    int[] choice = new int[MAX_SIZE];
+    int[] paintChoice = new int[MAX_SIZE];
+
+    double[] area = new double[MAX_SIZE];
+    double finalArea = 0;
+    double finalCost = 0;
+    int oil_Based = 120;
+    int Water_Based = 100;
+    int labour = 10;
+
     public int flag = 0;
-    char option;
-    
+    String[] option = new String[MAX_SIZE];
     Scanner read = new Scanner(System.in);
 
     public PaintCostEstimator() {
         System.out.print("Enter No. Of Rooms: ");
         No_of_rooms = read.nextInt();
         for (int count = 1; count <= No_of_rooms; count++) {
-            System.out.print("Enter Length of room " + count +": ");
+            System.out.print("Enter Length of room (in ft.)" + count + ": ");
             length[count] = read.nextDouble();
-            System.out.print("Enter Width of room " + count +": ");
+            System.out.print("Enter Width of room (in ft.)" + count + ": ");
             width[count] = read.nextDouble();
-            System.out.print("Select Height: 1. 8ft \n2. 9ft \n3. 10ft: ");
-            int choice = read.nextInt();
+            System.out.print("Select Height:8/9/10(in ft.)");
+            choice[count] = read.nextInt();
             System.out.print("Include Ceiling for room " + No_of_rooms + "(Y or N)? ");
-            option = read.next().charAt(0);
+            option[count] = read.next();
+            System.out.print("Select type of paint 1.Oil Based 2.Water Based");
+            paintChoice[count] = read.nextInt();
         }
 
     }
@@ -40,13 +51,35 @@ public class PaintCostEstimator {
         for (int index = 1; index <= No_of_rooms; index++) {
             System.out.println(length[index]);
             System.out.println(width[index]);
-            System.out.println(option);
+            System.out.println(option[index]);
+            System.out.println(finalCost);
         }
+    }
+
+    public void estimateCost() {
+        for (int count = 1; count <= No_of_rooms; count++) {
+            area[count] = (length[count] * choice[count]) + (width[count] * choice[count]);
+            if (option[count] == "y" || option[count] == "Y") {
+                area[count] = area[count] + (length[count] * width[count]);
+
+            }
+            finalArea = finalArea + area[count];
+
+            switch (paintChoice[count]) {
+                case 1:
+                    finalCost = oil_Based * finalArea * labour;
+                case 2:
+                    finalCost = Water_Based * finalArea * labour;
+            }
+
+        }
+
     }
 
     public static void main(String[] args) {
 
         PaintCostEstimator Estimate = new PaintCostEstimator();
+        Estimate.estimateCost();
         Estimate.display();
     }
 
