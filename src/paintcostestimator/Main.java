a87800e9acc4ce258ca54de4362414d2953f975b
BL01 <BL01@GIT-BL-11>
2017-07-13 13:56:38 +0530

    final

--- src/paintcostestimator/Main.java
+++ src/paintcostestimator/Main.java
@@ -1,12 +1,16 @@
 
 package paintcostestimator;
+/**
+ * Main part of the Code
+ * 
+ */
 
 public class Main {
 
     public static void main(String[] args) {
 
         PaintCostEstimator one = new PaintCostEstimator();
-        Calculation two = new Calculation();
+       
         one.NoOfRooms();                                                             
         one.roomDetails();
         one.height();
@@ -19,10 +23,10 @@ public class Main {
         double labourCost = one.labourCostEstimate();
         double totalCost = one.totalCostEstimate();
         double area = one.TotalroomArea();
-        System.out.println("Room Area= "+area+"Sq.ft");                             //display the total area
-        System.out.println("Labour Cost= "+labourCost+"Rs");                        //display the labour cost 
-        System.out.println("Painting cost= "+PaintType+"Rs");                       //display the paint cost
-        System.out.println("Total Cost= "+totalCost+"Rs");                          //display the total cost
+        System.out.println("Room Area= "+area+" Sq.ft");                             //display the total area
+        System.out.println("Labour Cost= "+labourCost+" Rs");                        //display the labour cost 
+        System.out.println("Painting cost= "+PaintType+" Rs");                       //display the paint cost
+        System.out.println("Total Cost= "+totalCost+" Rs");                          //display the total cost
     }
 
 }
